1f5584148d6b6ef927638452ee8bdcc693c9261c
GH120 <felipevieiraduarte195@gmail.com>
2025-05-31 17:48:00 -0300

    Prototipando possíveis observadores da partida (respondedor http e registrador de partidas)

---
+++ src/main/java/com/example/chess/api/GameEndHandler.java
@@ -0,0 +1,9 @@
+package com.example.chess.api;
+
+import com.example.chess.controlers.MatchObserver;
+
+//Vai lidar com o termino do jogo, desalocar partida e registrar ela numa database
+//Desenvolvimento Posterior, primeiro testar o MVP usando só o MatchEventDispacher
+// public class GameEndHandler implements MatchObserver{
+
+// }
